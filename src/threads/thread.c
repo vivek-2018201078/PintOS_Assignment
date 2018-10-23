38ad1c94c3698bbffee3c74402c0bc28a417e21d
Vivek Patare <vivek.patare@students.iiit.ac.in>
2018-10-23 05:43:15 +0530

    priority-change passed (thread.c->thread_set_priority)

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -380,7 +380,16 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  //thread_current ()->priority = new_priority;
+
+  ////////////////////////////////////////////////////////////////////
+
+
+  //checking preemption after current thread priority is changed
+  struct thread *current = thread_current();
+  current->priority = new_priority;
+  check_thread_preempt();
+  ////////////////////////////////////////////////////////////////////
 }
 
 /* Returns the current thread's priority. */
